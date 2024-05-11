411de866423c38a9a257919aef519b4a659e669e
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-05-12 00:42:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let subBtn = document.querySelector("#btn");
+let age = document.querySelector("#age");
+let name = document.querySelector("#name");
+subBtn.addEventListener('click', (e)=>{
+	e.preventDefault();
+	let fullname = name.value;
+	let currAge = parseInt(age.value);
+	if(currAge>18 && fullname!== undefined){
+		let makePromise = new Promise((resolve, reject)=>{
+			setTimeout(()=>{
+				resolve(alert(`Welcome, ${fullname}. You can vote`))
+			}, 4000)
+		})
+	}else{
+		let makePromise = new Promise((resolve, reject)=>{
+			setTimeout(()=>{
+				reject(alert(`Oh sorry, ${fullname}. You aren't old enough.`))
+			}, 4000)
+		})
+	}
+	
+})
